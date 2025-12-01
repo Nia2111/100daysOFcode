a32dd8a8af8122c0faa2c5fa5d322d54cf92571d
Nia2111 <niayadav13@gmail.com>
2025-12-01 12:27:09 +0530

    Create q68.c

---
+++ q68/q68.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    int expectedSum = n * (n + 1) / 2;
+
+    int missing = expectedSum - sum;
+
+    printf("%d", missing);
+
+    return 0;
+}
