bdeaf40aa26e3f4d208c4522a43b2bb4cf3a6dc8
Nia2111 <niayadav13@gmail.com>
2025-12-01 12:28:04 +0530

    Create q69.c

---
+++ q68/q69.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int visited[n + 1];   
+    for (int i = 0; i <= n; i++)
+        visited[i] = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (visited[arr[i]] == 1) {
+            printf("%d", arr[i]);
+            return 0;
+        }
+        visited[arr[i]] = 1;
+    }
+
+    return 0;
+}
