8e0a9f487771fddf5bc371da4f89bec485c81ebc
Nia2111 <niayadav13@gmail.com>
2025-12-01 10:36:24 +0530

    Update ques2.c

--- ques2.c
+++ ques2.c
@@ -1,7 +1,10 @@
-//Write a program to input two numbers and display their sum.
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
 #include<stdio.h>
 int main(){
-    int a=2, b=4;
-    printf("sum is %d", a+b);
+    int a=7, b=4;
+    printf("sum is %d\n",a+b);
+    printf("subs is %d\n", a-b);
+    printf("quotient is %d\n", a/b);
+    printf("mul is %d\n", a*b);
     return 0;
 }
